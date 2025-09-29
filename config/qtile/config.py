f1f9685b4b721cac410f0039895dcc4ec4355c86
wiizzl <github@houlliere.com>
2025-09-29 10:45:54 +0200

    fix: x11 drag rate

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -187,11 +187,7 @@ screens = [
                     foreground = colors[1],
                     padding = 5
                 ),
-                widget.WindowName(
-                    foreground = colors[6],
-                    padding = 8,
-                    max_chars = 40
-                ),
+                widget.Spacer(padding = 8),
                 widget.Systray(padding = 6),
                 sep,
                 widget.CPU(
@@ -231,7 +227,7 @@ screens = [
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
+        x11_drag_polling_rate = 60,
     ),
 ]
 
