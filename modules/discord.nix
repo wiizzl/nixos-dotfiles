dd31947fc064a76cfc39712b4a59e7b94aa25a58
wiizzl <github@houlliere.com>
2025-09-29 11:09:01 +0200

    feat: catppucin theme

--- modules/discord.nix
+++ modules/discord.nix
@@ -5,17 +5,13 @@
     enable = true;
     vesktop.enable = true;
     discord.enable = false;
-    quickCss = "
-    @import url('https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css');
-    ";
+    quickCss = "@import url('https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css');";
 
     config = {
       useQuickCss = true;
       frameless = true;
       plugins = {
-        fakeNitro = {
-          enable = true;
-        };
+        fakeNitro.enable = true;
       };
     };
   };
