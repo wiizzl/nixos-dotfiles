f41c2f0b91da9e657074dd27ddfa40101a884f12
wiizzl <github@houlliere.com>
2025-10-02 22:42:58 +0200

    fix: some config misstakes

--- home/programs/zen/default.nix
+++ home/programs/zen/default.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
+  imports = [ inputs.zen-browser.homeModules.beta ];
+
   programs.zen-browser = {
     enable = true;
     nativeMessagingHosts = [ pkgs.firefoxpwa ];
