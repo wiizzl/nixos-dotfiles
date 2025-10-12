2c2919f17f3591be7c8fd1f990c0949a24de7834
Pierre <66723882+wiizzl@users.noreply.github.com>
2025-10-12 12:58:44 +0200

    refactor: Git user configuration assignment

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,15 +1,11 @@
 { config, ... }:
 
-let
-  username = config.var.git.username;
-  email = config.var.git.email;
-in
 {
   programs.git = {
     enable = true;
 
-    userName = username;
-    userEmail = email;
+    userName = config.var.git.username;
+    userEmail = config.var.git.email;
 
     extraConfig = {
       init.defaultBranch = "main";
