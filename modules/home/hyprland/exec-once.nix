c13df4235af5b9ed0e533210beeae36b3b31ad0e
wiizzl <github@houlliere.com>
2025-10-21 14:23:15 +0200

    fix: ci

--- modules/home/hyprland/exec-once.nix
+++ modules/home/hyprland/exec-once.nix
@@ -3,7 +3,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      # "systemctl --user start hyprpolkitagent" # https://wiki.hypr.land/Hypr-Ecosystem/hyprpolkitagent
+
     ];
   };
 }
