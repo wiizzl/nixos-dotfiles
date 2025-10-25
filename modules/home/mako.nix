bc2c8571aca1fe29edda73e00b6c4478bf515a93
wiizzl <github@houlliere.com>
2025-10-25 12:31:15 +0200

    feat: enable mako service and simplify settings configuration

--- modules/home/mako.nix
+++ modules/home/mako.nix
@@ -2,23 +2,8 @@
 
 {
   services.mako = {
-    enable = false;
+    enable = true;
 
-    settings = {
-      "actionable=true" = {
-        anchor = "top-left";
-      };
-      actions = true;
-      anchor = "top-right";
-      border-radius = 6;
-      default-timeout = 5;
-      height = 100;
-      icons = true;
-      ignore-timeout = false;
-      layer = "top";
-      margin = 10;
-      markup = true;
-      width = 300;
-    };
+    settings = { };
   };
 }
--- modules/home/zen.nix
+++ modules/home/zen.nix
@@ -18,28 +18,55 @@
           }
         );
 
+        mkExtensionEntry =
+          {
+            id,
+            pinned ? false,
+          }:
+          let
+            base = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+              installation_mode = "force_installed";
+            };
+          in
+          if pinned then base // { default_area = "navbar"; } else base;
+
         mkExtensionSettings = builtins.mapAttrs (
-          _: pluginId: {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
-            installation_mode = "force_installed";
-          }
+          _: entry: if builtins.isAttrs entry then entry else mkExtensionEntry { id = entry; }
         );
       in
       {
         Preferences = mkLockedAttrs {
           "browser.tabs.warnOnClose" = false;
-          "zen.tabs.vertical.right-side" = true;
-          "zen.view.use-single-toolbar" = false;
+          "browser.aboutConfig.showWarning" = false;
+          "media.videocontrols.picture-in-picture.video-toggle.enabled" = true;
+          "browser.gesture.swipe.left" = "";
+          "browser.gesture.swipe.right" = "";
+          "browser.tabs.hoverPreview.enabled" = true;
         };
 
         ExtensionSettings = mkExtensionSettings {
-          "@react-devtools" = "react-developer-tools";
-          "uBlock0@raymondhill.net" = "ublock-origin";
-          "wappalyzer@crunchlabz.com" = "wappalyzer";
+          "wappalyzer@crunchlabz.com" = mkExtensionEntry {
+            id = "wappalyzer";
+            pinned = true;
+          };
+          "uBlock0@raymondhill.net" = mkExtensionEntry {
+            id = "ublock-origin";
+            pinned = true;
+          };
           "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
+          "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = "return-youtube-dislikes";
+          "{74145f27-f039-47ce-a470-a662b129930a}" = "clearurls";
+          "github-no-more@ihatereality.space" = "github-no-more";
+          "github-repository-size@pranavmangal" = "gh-repo-size";
+          "@searchengineadremover" = "searchengineadremover";
+          "jid1-BoFifL9Vbdl2zQ@jetpack" = "decentraleyes";
+          "trackmenot@mrl.nyu.edu" = "trackmenot";
           "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = "stylus";
+          "@react-devtools" = "react-developer-tools";
         };
 
+        PasswordManagerEnabled = false;
         AutofillAddressEnabled = false;
         AutofillCreditCardEnabled = false;
         DisableAppUpdate = true;
@@ -48,7 +75,6 @@
         DisablePocket = true;
         DisableTelemetry = true;
         DontCheckDefaultBrowser = true;
-        NoDefaultBookmarks = true;
         OfferToSaveLogins = false;
         EnableTrackingProtection = {
           Value = true;
@@ -61,6 +87,42 @@
     profiles."default" = {
       containersForce = true;
       spacesForce = true;
+      pinsForce = true;
+
+      settings = {
+        "zen.tabs.vertical.right-side" = true;
+        "zen.view.use-single-toolbar" = false;
+        "zen.view.compact.animate-sidebar" = false;
+        "zen.workspaces.continue-where-left-off" = true;
+        "zen.workspaces.natural-scroll" = true;
+        "zen.view.compact.hide-tabbar" = true;
+        "zen.view.compact.hide-toolbar" = false;
+        "zen.welcome-screen.seen" = true;
+      };
+
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            name = "Nix sites";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "homepage";
+                url = "https://nixos.org/";
+              }
+              {
+                name = "wiki";
+                tags = [
+                  "wiki"
+                  "nix"
+                ];
+                url = "https://wiki.nixos.org/";
+              }
+            ];
+          }
+        ];
+      };
 
       containers = {
         Work = {
@@ -80,6 +142,78 @@
           container = 1;
           position = 2000;
         };
+      };
+
+      search = {
+        force = true;
+        default = "google";
+        engines =
+          let
+            nixSnowflakeIcon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          in
+          {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = nixSnowflakeIcon;
+              definedAliases = [ "np" ];
+            };
+            "Nix Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = nixSnowflakeIcon;
+              definedAliases = [ "nop" ];
+            };
+            "Home Manager Options" = {
+              urls = [
+                {
+                  template = "https://home-manager-options.extranix.com/";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                    {
+                      name = "release";
+                      value = "master"; # unstable
+                    }
+                  ];
+                }
+              ];
+              icon = nixSnowflakeIcon;
+              definedAliases = [ "hmop" ];
+            };
+          };
       };
     };
   };
