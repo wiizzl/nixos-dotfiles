3880a0c249e8e379555079e5adb668306b41e27e
wiizzl <github@houlliere.com>
2025-10-28 20:30:24 +0100

    refactor: reorganize Spotify configuration for clarity and structure

--- modules/home/apps/spotify/default.nix
+++ modules/home/apps/spotify/default.nix
@@ -9,11 +9,8 @@
 with lib;
 let
   inherit (config.my) apps user;
-
-  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in
 {
-  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
   options.my.apps.spotify = {
     enable = mkEnableOption "Enable Spotify with Spicetify";
@@ -21,14 +18,20 @@ in
 
   config = mkIf apps.spotify.enable {
     home-manager.users.${user.name} = {
-      programs.spicetify = {
-        enable = true;
+      imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-        enabledExtensions = with spicePkgs.extensions; [
-          hidePodcasts
-          adblock
-        ];
-      };
+      programs.spicetify =
+        let
+          spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+        in
+        {
+          enable = true;
+
+          enabledExtensions = with spicePkgs.extensions; [
+            hidePodcasts
+            adblock
+          ];
+        };
     };
   };
 }
