ede7567e40bc89f782c17a05b7ced71de55bcf16
wiizzl <github@houlliere.com>
2025-10-29 15:34:26 +0100

    feat: add MAUI development shell configuration with necessary build inputs

--- shells/maui/default.nix
+++ shells/maui/default.nix
@@ -4,15 +4,23 @@
   perSystem =
     { pkgs, ... }:
     {
-      devShells.cs = pkgs.mkShell {
+      devShells.maui = pkgs.mkShell {
         buildInputs = with pkgs; [
           dotnetCorePackages.sdk_9_0-bin
           dotnetCorePackages.runtime_9_0-bin
           dotnetPackages.Nuget
         ];
 
+        postInstall = ''
+          if [ ! -w "$HOME" ]; then
+            export HOME=$(mktemp -d)
+          fi
+
+          $out/bin/dotnet workload install maui-android
+        '';
+
         shellHook = ''
-          echo "C# dev shell is ready !"
+          echo "C# + MAUI dev shell is ready !"
         '';
       };
     };
